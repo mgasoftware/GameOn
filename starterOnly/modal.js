ee7d1af1a762082f446f981cf69f577c90381652
mgasoftware <macir.gassama12@gmail.com>
2022-06-25 17:42:24 +0200

    Add confirmation form and check error for name and email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,12 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const form = document.getElementById('form');
+const confirmationModal = document.querySelector('.submit-confirmation-container');
+const close = document.getElementsByClassName('close');
+const firstName = document.getElementById('first');
+const lastName = document.getElementById('last');
+const email = document.getElementById('email');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +26,48 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
+function closeForm() {
+  modalbg.style.display = "none";
+}
+close[0].addEventListener('click', closeForm);
 
+// first and last name check
+function checkFirstName() {
+  if (firstName.value.trim().length < 2 || firstName.value.trim() === '') {
+    firstName.parentElement.setAttribute('data-error-visible', 'true');
+    firstName.style.border = '2px solid red';
+    return false;
+  }
+  firstName.parentElement.setAttribute('data-error-visible', 'false');
+  return true;
+}
+function checkLastName() {
+  if (lastName.value.trim().length < 2 || lastName.value.trim() == '') {
+    lastName.parentElement.setAttribute('data-error-visible', 'true');
+    return false;
+  }
+  lastName.parentElement.setAttribute('data-error-visible', 'false');
+  return true;
+}
+
+// email check
+function checkEmail() {
+  var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if(email.value.trim().match(regex)){
+    email.parentElement.setAttribute('data-error-visible', 'false');
+    return true;
+  }
+  email.parentElement.setAttribute('data-error-visible', 'true');
+  return false;
+}
+
+// send form
+const formSubmit = e => {
+  e.preventDefault();
+  if (checkFirstName() && checkLastName() && checkEmail()) {
+    confirmationModal.style.display = "block";
+  }
+}
+
+form.addEventListener('submit', formSubmit);
