bbb7fc8146bb22fccbfd8eede13b6833326f3519
mgasoftware <macir.gassama12@gmail.com>
2022-06-27 16:18:44 +0200

    Integrate forms entry

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -17,6 +17,12 @@ const close = document.getElementsByClassName('close');
 const firstName = document.getElementById('first');
 const lastName = document.getElementById('last');
 const email = document.getElementById('email');
+const birthDate = document.getElementById('birthdate');
+const quantityTournament = document.getElementById('quantity');
+const checkboxLocation = document.getElementById('allLocation');
+const locations = document.querySelectorAll('#allLocation .checkbox-input');
+const cguValid = document.getElementById('checkbox1');
+const closeBtn = document.getElementsByClassName('submit-confirmation__btn');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -31,12 +37,12 @@ function closeForm() {
   modalbg.style.display = "none";
 }
 close[0].addEventListener('click', closeForm);
+close[1].addEventListener('click', closeForm);
 
 // first and last name check
 function checkFirstName() {
   if (firstName.value.trim().length < 2 || firstName.value.trim() === '') {
     firstName.parentElement.setAttribute('data-error-visible', 'true');
-    firstName.style.border = '2px solid red';
     return false;
   }
   firstName.parentElement.setAttribute('data-error-visible', 'false');
@@ -54,7 +60,7 @@ function checkLastName() {
 // email check
 function checkEmail() {
   var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if(email.value.trim().match(regex)){
+  if (email.value.trim().match(regex)) {
     email.parentElement.setAttribute('data-error-visible', 'false');
     return true;
   }
@@ -62,12 +68,78 @@ function checkEmail() {
   return false;
 }
 
+//birthdate check
+function checkBirthDate() {
+  if (birthDate.value.trim().length !== 10) {
+    birthDate.parentElement.setAttribute('data-error-visible', 'true');
+    return false
+  }
+  birthDate.parentElement.setAttribute('data-error-visible', 'false');
+  return true;
+}
+
+//tournament quantity check
+function checkQuantityTournament() {
+  if (quantityTournament.value.trim() == '') {
+    quantityTournament.parentElement.setAttribute('data-error-visible', 'true');
+    return false;
+  }
+  quantityTournament.parentElement.setAttribute('data-error-visible', 'false');
+  return true;
+}
+
+//location check
+function checkCheckboxLocation() {
+  for (let i = 0; i < locations.length; i++) {
+    if (locations[i].checked) {
+      checkboxLocation.setAttribute('data-error-visible', 'false');
+      return true;
+    }
+    checkboxLocation.setAttribute('data-error-visible', 'true');
+  }
+  return false;
+}
+
+//CGU check
+function checkCGUValid() {
+  if(cguValid.checked){
+    cguValid.parentElement.setAttribute('data-error-visible', 'false');
+    return true;
+  }
+  cguValid.parentElement.setAttribute('data-error-visible', 'true');
+  return false;
+}
+
+//function who scan the forms entry
+function checkAll() {
+  checkFirstName();
+  checkLastName();
+  checkEmail();
+  checkBirthDate();
+  checkQuantityTournament();
+  checkCheckboxLocation();
+  checkCGUValid();
+}
+
+//function who see if the form entry are valid
+function formValid() {
+  if (checkFirstName() && checkLastName() && checkEmail() && checkBirthDate()
+    && checkQuantityTournament() && checkCheckboxLocation() && checkCGUValid()) {
+    return true;
+  }
+  return false;
+}
+
 // send form
 const formSubmit = e => {
   e.preventDefault();
-  if (checkFirstName() && checkLastName() && checkEmail()) {
+  if (formValid()) {
     confirmationModal.style.display = "block";
   }
+  else checkAll();
 }
 
 form.addEventListener('submit', formSubmit);
+
+//close form button when is end
+closeBtn[0].addEventListener('click', closeForm);
